535cc984c0cb65e63dfd95001261158857520b2a
eHeuristic <vivekjotangiya2003@gmail.com>
2023-06-02 15:35:58 +0530

    second commit

--- app/src/main/java/com/example/social_mediavj/MainActivity.kt
+++ app/src/main/java/com/example/social_mediavj/MainActivity.kt
@@ -1,17 +1,55 @@
 package com.example.social_mediavj
 
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.social_mediavj.databinding.ActivityMainBinding
+import com.example.social_mediavj.ui.HomeFragment
+import com.example.social_mediavj.ui.NotificationsFragment
+import com.example.social_mediavj.ui.ProfileFragment
+import com.example.social_mediavj.ui.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    internal var selectedfragment: Fragment? = null
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                selectedfragment = HomeFragment()
+            }
+            R.id.nav_search -> {
+                selectedfragment = SearchFragment()
+            }
+            R.id.nav_add_post -> {
+                item.isChecked = false
+               // startActivity(Intent(this@MainActivity, AddPostActivity::class.java))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notification -> {
+                selectedfragment = NotificationsFragment()
+            }
+            R.id.nav_profile -> {
+                selectedfragment = ProfileFragment()
+            }
+        }
+
+        if(selectedfragment != null){
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                selectedfragment!!
+            ).commit()
+        }
+
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +59,20 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+       // moveToFragment(HomeFragment())
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            HomeFragment()
+        ).commit()
+
+    }
+
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+      //  fragmentTrans.replace(R.id.fragment_container, fragment)
+      //  fragmentTrans.commit()
     }
 }
